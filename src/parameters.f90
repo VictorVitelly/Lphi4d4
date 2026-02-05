446eb58ad79c40ef46bee589586e47ba86ea0615
VictorVitelly_ICN <victor-vitelly@ciencias.unam.mx>
2026-02-05 14:05:22 -0600

    Ready to make namelist."
    exit

--- src/parameters.f90
+++ src/parameters.f90
@@ -2,9 +2,9 @@ module parameters
 
     use iso_fortran_env, only : dp => real64, i32 => int32
     implicit none
-    integer(i32), parameter :: L=6,thermalization=5000,eachsweep=100
+    integer(i32), parameter :: L=10,thermalization=5000,eachsweep=100
     real(dp), parameter :: lamb0=1.0_dp
-    integer(i32) :: Nmsrs=100, Nmsrs2=120
+    integer(i32) :: Nmsrs=30, Nmsrs2=120
     real(dp) :: dphi=0.4_dp, hotphi=1._dp
     integer(i32), parameter :: Mbin(5)=(/4,5,10,15,20/)
 
